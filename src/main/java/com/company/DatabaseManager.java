dba22bdc3c925b640bad926a14ca40742fe2dc34
Gusts Linkevics <gusts@gst.lv>
2021-03-25 21:38:11 +0200

    Initial commit

---
+++ src/main/java/com/company/DatabaseManager.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseManager {
+    private static SessionFactory factory;
+
+    public DatabaseManager() {
+        try {
+            factory = new Configuration()
+                    .configure()
+                    .addAnnotatedClass(Country.class)
+                    .buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public List<Country> getCountries() {
+        var session = factory.openSession();
+
+        try {
+            return session.createQuery("FROM Country").list();
+        } catch (HibernateException ex) {
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+
+        return new ArrayList<>();
+    }
+}
---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+      var db = new DatabaseManager();
+
+       var result = db.getCountries();
+
+        for (var country :
+                result) {
+            System.out.println(country.getName());
+        }
+    }
+}
