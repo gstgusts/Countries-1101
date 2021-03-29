dbea14f74fb384e3abe12ab487fffd3480d5c9d0
Gusts Linkevics <gusts@gst.lv>
2021-03-29 20:10:51 +0300

    Added add, update, delete getById ,method

--- src/main/java/com/company/DatabaseManager.java
+++ src/main/java/com/company/DatabaseManager.java
@@ -2,6 +2,7 @@ package com.company;
 
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import java.util.ArrayList;
@@ -34,5 +35,81 @@ public class DatabaseManager {
         }
 
         return new ArrayList<>();
+    }
+
+    public void save(Object item) {
+        var session = factory.openSession();
+        Transaction tx = null;
+
+        try {
+           tx = session.beginTransaction();
+           session.save(item);
+           tx.commit();
+        } catch (HibernateException ex) {
+            if(tx != null) {
+                tx.rollback();
+            }
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+    }
+
+    public void update(Object item) {
+        var session = factory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            session.update(item);
+            tx.commit();
+        } catch (HibernateException ex) {
+            if(tx != null) {
+                tx.rollback();
+            }
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+    }
+
+    public void updateCountry(Country country) {
+        if(country.getId() == 0) {
+            return;
+        }
+
+        update(country);
+    }
+
+    public void delete(Object item) {
+        var session = factory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            session.delete(item);
+            tx.commit();
+        } catch (HibernateException ex) {
+            if(tx != null) {
+                tx.rollback();
+            }
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+    }
+
+    public Country getById(int id) {
+        var session = factory.openSession();
+
+        try {
+            return session.get(Country.class, id);
+        } catch (HibernateException ex) {
+            System.err.println(ex);
+        } finally {
+            session.close();
+        }
+
+        return null;
     }
 }
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,14 +1,41 @@
 package com.company;
 
+import org.junit.Test;
+
 public class Main {
     public static void main(String[] args) {
       var db = new DatabaseManager();
 
+      var country1 = new Country(0, "GG", "Funny name 3");
+      db.save(country1);
+
+       System.out.println(country1.getId());
+
        var result = db.getCountries();
+
+//       var countryToUpdate = result.get(result.size() - 1);
+//       countryToUpdate.setName("Funny name 2 - 1");
+//       db.update(countryToUpdate);
 
         for (var country :
                 result) {
             System.out.println(country.getName());
         }
+    }
+
+    @Test
+    public void delete() {
+        var dm = new DatabaseManager();
+        var result = dm.getCountries();
+
+        var itemToDelete = result.get(result.size() - 1);
+
+        dm.delete(itemToDelete);
+    }
+
+    @Test
+    public void getById() {
+        var dm = new DatabaseManager();
+        dm.getById(1);
     }
 }
